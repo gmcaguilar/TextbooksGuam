ced251b705d8a78523b7fd1f036b823c6a2340d8
jasncani <jasonbacani@gmail.com>
2017-09-04 20:59:14 +1000

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 jason = User.create(first_name: 'Jason', last_name: 'Bacani', email: 'jason@email.com', password: 'jason', password_confirmation: 'jason')
-Post.create(title: 'Making Sense of Micronesia', authors: 'Francis X. Hezel', isbn13: '9780824836610', description: 'Good', user_id: jason.id)
+Post.create([{title: 'Microsoft Access 2013: Comprehensive', authors: 'Pratt & Last', isbn10: '1285168968', isbn13: '9781285168968', description: 'Good', user_id: jason.id},{title: "Writing your way through College: a Student's Guide", authors: 'Fontaine and Smith', isbn10: '0867095911', isbn13: '9780867095913', description: 'Good', user_id: jason.id},{title: 'Philosophy of Religion: an Anthology', authors: 'Rea and Pojman', isbn10: '1285197321', isbn13: '9781285197326', description: 'Good', user_id: jason.id}])
