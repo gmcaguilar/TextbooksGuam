a46fdcdcc909b2af20453eb8c2d7b904c52f7847
jasncani <jasonbacani@gmail.com>
2017-09-06 11:49:48 +1000

    Add show action for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,10 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def show
+		@post = Post.find(params[:id])
+	end
+
 	private
 		def post_params
 			params.require(:post).permit(:title, :authors, :isbn10, :isbn13, :description, :price)
