beaaedc0567260e91e91a4ba9ed9b08b3114574c
jasncani <jasonbacani@gmail.com>
2017-09-06 21:18:00 +1000

    Make posts only editable by user

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,7 @@ class Post < ApplicationRecord
 	belongs_to :user
 
 	def editable?(user)
-		if user == nil
-			false
-		else
-			user.admin? || self.user == user
-		end
+		user == nil && self.user == user
 	end
 
 end
