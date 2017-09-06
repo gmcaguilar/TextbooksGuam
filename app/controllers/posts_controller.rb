07fa87d9599964cc500ca7bee807d44fb399478b
jasncani <jasonbacani@gmail.com>
2017-09-06 10:21:15 +1000

    Add post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,28 @@
 class PostsController < ApplicationController
 
+	before_action :require_user, only: [:new, :create]
+
 	def index
 		@posts = Post.all
 	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		@post.user_id = session[:user_id]
+		if (@post.save)
+			redirect_to '/'
+		else
+			redirect_to 'create'
+		end
+	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :authors, :isbn10, :isbn13, :description, :price)
+		end
 
 end
